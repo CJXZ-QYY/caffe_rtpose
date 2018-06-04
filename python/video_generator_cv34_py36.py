86179b325c35e66f23c3d6519abe9d8a4a6298f5
ChingChing <Quentinyq@163.com>
2018-06-04 17:37:59 +0800

    change as genenral format

--- python/video_generator_cv34_py36.py
+++ python/video_generator_cv34_py36.py
@@ -1,15 +1,16 @@
 import cv2
 
 fps = 24   
-#fourcc = cv2.VideoWriter_fourcc('M','J','P','G')  
-fourcc = cv2.VideoWriter_fourcc('M','P','4','2')  
-#fourcc = cv2.VideoWriter_fourcc('D','I','V','X')  
-#fourcc = cv2.VideoWriter_fourcc('I','4','2','0')  
-videoWriter = cv2.VideoWriter('/home/king/video_maker/posepic/fh_result1.mp4', fourcc, fps, (1280,720))  
-for i in range(0,4102):
-
-    frame = cv2.imread('/home/king/video_maker/posepic/fh_output_full/frame'+str(i).zfill(6)+'.jpg')
-    #print (frame)
+frame_size=(1280,720)
+total_frame_num=1000
+video_out_dir='/home/[username]/video_maker/posepic/result.mp4'
+#fourcc = cv2.VideoWriter_fourcc('M','J','P','G')  #for .mp4 output
+fourcc = cv2.VideoWriter_fourcc('D','I','V','X')  #for .mp4 output
+#fourcc = cv2.VideoWriter_fourcc('I','4','2','0')  #for .avi output
+videoWriter = cv2.VideoWriter(video_out_dir, fourcc, fps, frame_size)  
+for i in range(0,total_frame_num):
+    frame = cv2.imread('/home/[dir_to_images]/'+str(i).zfill(6)+'.jpg')
+    #print (str(i).zfill(6))
 #    cv2.imshow('img', img12)
 #    cv2.waitKey(1000/int(fps))
     videoWriter.write(frame)
